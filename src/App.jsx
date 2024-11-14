aa77eddfabc5b26122252e64e21f56220d43b804
hlimhli <34466635+hlimhli@users.noreply.github.com>
2024-11-14 20:09:31 +0800

    1111

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Home from './Home';
+import Register from './Register';
 import About from './About';
 import {Route, Switch} from 'wouter';
 import './App.css'
@@ -12,6 +13,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" component={Home} />
+        <Route path="/register" component={Register} />
         <Route path="/about" component={About} />
       </Switch>
       <Footer />
